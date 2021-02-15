62a18d7ec3d75ea975adbd5044bad9b84bf7cdb5
Eduardo Laguna <eduardo.laguna@mercadolibre.cl>
2021-02-15 16:54:03 -0300

    Updates name for struct

--- arrays/tarea.go
+++ arrays/tarea.go
@@ -5,6 +5,6 @@ package arrays
 //Si tenemos availableClassrooms = 3 y 15 estudiantes entonces deberíamos tener 3 listas con 5 estudiantes en cada una.
 //para hacer esto debes refrescar tus conocomientos para crear estructuras y poder conocer su nombre, apellido y edad.
 //También recordad lo que vimos en clases sobre poder dividir slice
-func split(students []students, availableClassrooms uint) ([][]students, error) {
-    panic("TODO: implement function")
+func split(Students []Students, availableClassrooms uint) ([][]Students, error) {
+	panic("TODO: implement function")
 }
--- arrays/tarea_test.go
+++ arrays/tarea_test.go
@@ -1,60 +1,60 @@
 package arrays
 
 import (
-    "reflect"
-    "testing"
+	"reflect"
+	"testing"
 )
 
 func Test_split(t *testing.T) {
-    type args struct {
-        students            []students
-        availableClassrooms uint
-    }
-    tests := []struct {
-        name    string
-        args    args
-        want    [][]students
-        wantErr bool
-    }{
-        {name: "Retorna 3 listas de estudiantes", args: args{
-            students: []students{{"Pedro", "Zamora", 15}, {"Mariela", "Fernandez", 16}, {"Angel", "Navarro", 15}, {"Ana", "Cuenca", 16}, {"Monica", "Flores", 14}, {"Carlos", "Alvarado", 15}},
-            availableClassrooms: 3,
-        }, want: [][]students{
-            {{"Pedro", "Zamora", 15}, {"Mariela", "Fernandez", 16}},
-            {{"Angel", "Navarro", 15}, {"Ana", "Cuenca", 16}},
-            {{"Monica", "Flores", 14}, {"Carlos", "Alvarado", 15}},
-        }, wantErr: false},
-        {name: "Retorna 2 listas de estudiantes", args: args{
-            students: []students{{"Pedro", "Zamora", 15}, {"Mariela", "Fernandez", 16}, {"Angel", "Navarro", 15}, {"Ana", "Cuenca", 16}, {"Monica", "Flores", 14}, {"Carlos", "Alvarado", 15}},
-            availableClassrooms: 2,
-        }, want: [][]students{
-            {{"Pedro", "Zamora", 15}, {"Mariela", "Fernandez", 16}, {"Angel", "Navarro", 15}},
-            {{"Ana", "Cuenca", 16}, {"Monica", "Flores", 14}, {"Carlos", "Alvarado", 15}},
-        }, wantErr: false},
-        {name: "Retorna 4 listas de estudiantes", args: args{
-            students: []students{{"Pedro", "Zamora", 15}, {"Mariela", "Fernandez", 16}, {"Angel", "Navarro", 15}, {"Ana", "Cuenca", 16}, {"Monica", "Flores", 14}, {"Carlos", "Alvarado", 15}},
-            availableClassrooms: 4,
-        }, want: [][]students{
-            {{"Pedro", "Zamora", 15}, {"Mariela", "Fernandez", 16}},
-            {{"Angel", "Navarro", 15}, {"Ana", "Cuenca", 16}},
-            {{"Monica", "Flores", 14}},
-            {{"Carlos", "Alvarado", 15}},
-        }, wantErr: false},
-        {name: "Retorna error", args: args{
-            students: nil,
-            availableClassrooms: 4,
-        }, want: nil, wantErr: true},
-    }
-    for _, tt := range tests {
-        t.Run(tt.name, func(t *testing.T) {
-            got, err := split(tt.args.students, tt.args.availableClassrooms)
-            if (err != nil) != tt.wantErr {
-                t.Errorf("split() error = %v, wantErr %v", err, tt.wantErr)
-                return
-            }
-            if !reflect.DeepEqual(got, tt.want) {
-                t.Errorf("split() got = %v, want %v", got, tt.want)
-            }
-        })
-    }
+	type args struct {
+		Students            []Students
+		availableClassrooms uint
+	}
+	tests := []struct {
+		name    string
+		args    args
+		want    [][]Students
+		wantErr bool
+	}{
+		{name: "Retorna 3 listas de estudiantes", args: args{
+			Students:            []Students{{"Pedro", "Zamora", 15}, {"Mariela", "Fernandez", 16}, {"Angel", "Navarro", 15}, {"Ana", "Cuenca", 16}, {"Monica", "Flores", 14}, {"Carlos", "Alvarado", 15}},
+			availableClassrooms: 3,
+		}, want: [][]Students{
+			{{"Pedro", "Zamora", 15}, {"Mariela", "Fernandez", 16}},
+			{{"Angel", "Navarro", 15}, {"Ana", "Cuenca", 16}},
+			{{"Monica", "Flores", 14}, {"Carlos", "Alvarado", 15}},
+		}, wantErr: false},
+		{name: "Retorna 2 listas de estudiantes", args: args{
+			Students:            []Students{{"Pedro", "Zamora", 15}, {"Mariela", "Fernandez", 16}, {"Angel", "Navarro", 15}, {"Ana", "Cuenca", 16}, {"Monica", "Flores", 14}, {"Carlos", "Alvarado", 15}},
+			availableClassrooms: 2,
+		}, want: [][]Students{
+			{{"Pedro", "Zamora", 15}, {"Mariela", "Fernandez", 16}, {"Angel", "Navarro", 15}},
+			{{"Ana", "Cuenca", 16}, {"Monica", "Flores", 14}, {"Carlos", "Alvarado", 15}},
+		}, wantErr: false},
+		{name: "Retorna 4 listas de estudiantes", args: args{
+			Students:            []Students{{"Pedro", "Zamora", 15}, {"Mariela", "Fernandez", 16}, {"Angel", "Navarro", 15}, {"Ana", "Cuenca", 16}, {"Monica", "Flores", 14}, {"Carlos", "Alvarado", 15}},
+			availableClassrooms: 4,
+		}, want: [][]Students{
+			{{"Pedro", "Zamora", 15}, {"Mariela", "Fernandez", 16}},
+			{{"Angel", "Navarro", 15}, {"Ana", "Cuenca", 16}},
+			{{"Monica", "Flores", 14}},
+			{{"Carlos", "Alvarado", 15}},
+		}, wantErr: false},
+		{name: "Retorna error", args: args{
+			Students:            nil,
+			availableClassrooms: 4,
+		}, want: nil, wantErr: true},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got, err := split(tt.args.Students, tt.args.availableClassrooms)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("split() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("split() got = %v, want %v", got, tt.want)
+			}
+		})
+	}
 }
